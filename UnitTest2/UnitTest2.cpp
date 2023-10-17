ee09965acbfcb4d439e8375898759d9ecd1951f5
Mykhailo <mykhailo.davydenko.ik.2023@lpnu.ua>
2023-10-17 13:46:15 +0300

    test_commit

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba_5.0/Laba_5.0.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = F(1, 1 ,1);
+			Assert::AreEqual(t, 0.236024, 0.00001);
+		}
+
+	};
+}
